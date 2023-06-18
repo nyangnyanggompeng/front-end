bedbbc1467db95f69f573bd859d1b318d157fd81
yoouyeon <jyeon.yoon59@gmail.com>
2023-06-18 22:42:52 +0900

    [FEAT] 닉네임 확인 컴포넌트 추가 #1

--- src/components/SignIn/NicknameCheck.tsx
+++ src/components/SignIn/NicknameCheck.tsx
@@ -4,10 +4,18 @@ type NicknameCheckProps = {
   status?: NicknameCheckStatus;
 };
 
+const statusMessage: Record<NicknameCheckStatus, string> = {
+  OK: '사용 가능한 닉네임입니다.',
+  DUPLICATED: '이미 사용 중인 닉네임입니다.',
+  INVALID: '사용 불가능한 닉네임입니다.',
+};
+
 function NicknameCheck({ status }: NicknameCheckProps) {
+  const message = status ? statusMessage[status] : '';
   return (
     <div>
       <input type='text' name='nickname' placeholder='닉네임' />
+      <div>{message}</div>
       <button>중복 확인</button>
     </div>
   );
