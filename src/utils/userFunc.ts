87e6a9d58090e27ba7607425ea3e177c1b2bc197
moondrop0816 <moondrop0816@gmail.com>
2023-06-22 18:19:03 +0900

    [CHORE] 로그아웃 함수 로직 틀 작성

--- src/utils/userFunc.ts
+++ src/utils/userFunc.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 // 이메일 체크 로직
 export const checkEmail = (email: string): boolean => {
   const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
@@ -11,3 +13,9 @@ export const checkPw = (pw: string): boolean => {
 };
 
 // 로그아웃 로직
+export const logoutHandler = () => {
+  return axios
+    .get('http://localhost:4000')
+    .then((res) => console.log('로그아웃시 상태변경'))
+    .catch((err) => console.log('로그아웃 실패 로직'));
+};
