feee7cf9b39c5d5a9ae9c8312ce6d314e7ee572e
moondrop0816 <moondrop0816@gmail.com>
2023-06-22 15:34:22 +0900

    [CHORE] 내용수정

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,21 +1,13 @@
 import { useState } from 'react';
-import { checkEmail, checkPw } from '../utils/userFunc';
 
-interface User {
+interface LoginInfo {
   email: string;
   password: string;
 }
 
-enum ErrMsg {
-  None = '',
-  Email = '이메일을 입력해주세요.',
-  Pw = '비밀번호를 입력해주세요.',
-  Both = '이메일 또는 비밀번호가 잘못 입력되었습니다.',
-}
-
 const SignIn = () => {
-  const [error, setError] = useState<ErrMsg>(ErrMsg.None);
-  const [user, setUser] = useState<User>({
+  const [error, setError] = useState(false);
+  const [loginInfo, setLoginInfo] = useState<LoginInfo>({
     email: '',
     password: '',
   });
@@ -23,48 +15,42 @@ const SignIn = () => {
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
 
-    setUser({
-      ...user,
+    setLoginInfo({
+      ...loginInfo,
       [id]: value,
     });
   };
 
-  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+  const loginRequestHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     // 로그인 이벤트 로직
     e.preventDefault();
 
     // 입력된 값 유효성 검사
-    if (user.email === '') {
-      setError(ErrMsg.Email);
+    if (!loginInfo.email || !loginInfo.password) {
+      setError(true);
       return;
     }
-    if (user.password === '') {
-      setError(ErrMsg.Pw);
-      return;
-    }
-    if (!checkEmail(user.email) || !checkPw(user.password)) {
-      setError(ErrMsg.Both);
-      return;
-    }
-
-    setError(ErrMsg.None);
-    console.log('로그인');
 
     // 액세스 토큰 발급(로그인)
     // 발급된 토큰으로 유저정보 가져오기
     // 필요한 페이지로 리다이렉트
+    console.log('토큰 발급');
+    console.log('유저 정보 가져와서 리덕스 툴킷으로 스토어에 저장');
+    console.log('메인/gpt페이지로 리다이렉트');
+
+    console.log('로그인');
   };
 
   return (
     <div>
-      <form onSubmit={handleLogin}>
+      <form onSubmit={loginRequestHandler}>
         <div>
           <label htmlFor='email'>이메일</label>
           <input
             type='text'
             id='email'
             placeholder='abcd@email.com'
-            value={user.email}
+            value={loginInfo.email}
             onChange={handleOnChange}
           />
         </div>
@@ -74,11 +60,13 @@ const SignIn = () => {
             type='text'
             id='password'
             placeholder='password'
-            value={user.password}
+            value={loginInfo.password}
             onChange={handleOnChange}
           />
         </div>
-        {error && <p className='err-msg'>{error}</p>}
+        {error && (
+          <p className='err-msg'>이메일 또는 비밀번호가 잘못 입력되었습니다.</p>
+        )}
         <button type='submit'>로그인</button>
       </form>
       <p>
