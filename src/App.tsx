dd97128353271d2d8fc59617112fa2ab1665f80b
moondrop0816 <moondrop0816@gmail.com>
2023-06-24 22:55:16 +0900

    [CHORE] axios 전역 기본값 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import Router from './Router';
+import axios from 'axios';
 
 function App() {
+  // axios 전역 기본값 설정
+  axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
+  axios.defaults.headers.post['Content-Type'] = 'application/json';
+  axios.defaults.withCredentials = true;
+
   return (
     <>
       <Router />
