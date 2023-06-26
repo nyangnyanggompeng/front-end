3c79a7488bdd8c2d1cb6f64d953631a5ef257e13
moondrop0816 <moondrop0816@gmail.com>
2023-06-26 16:04:45 +0900

    [FIX] 환경변수 수정 #2

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import Router from './Router';
 import axios from 'axios';
 
 function App() {
+  const { VITE_SERVER_URL } = import.meta.env;
   // axios 전역 기본값 설정
-  axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
+  axios.defaults.baseURL = VITE_SERVER_URL;
   axios.defaults.headers.post['Content-Type'] = 'application/json';
   axios.defaults.withCredentials = true;
 
