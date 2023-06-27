32cb45e40530298b761f0add67726f553a635b92
yoouyeon <jyeon.yoon59@gmail.com>
2023-06-27 23:26:41 +0900

    [FIX] ModalOverlay에서 발생한 click이벤트만 핸들링하도록 수정 #3

--- src/components/Modal/ModalContainer.tsx
+++ src/components/Modal/ModalContainer.tsx
@@ -9,8 +9,13 @@ type ModalProps = {
 };
 
 export function ModalContainer({ resetModal, children }: ModalProps) {
+  function onClick(e: React.MouseEvent<HTMLDivElement>) {
+    if (e.target === e.currentTarget) {
+      resetModal();
+    }
+  }
   return (
-    <div css={ModalOverlayStyles} onClick={resetModal}>
+    <div css={ModalOverlayStyles} onClick={onClick}>
       <div css={ModalContainerStyles}>{children}</div>
     </div>
   );
