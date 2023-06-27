c0ffff5c9e52555e3fe8636fadb1a965a030afc9
yoouyeon <jyeon.yoon59@gmail.com>
2023-06-27 23:27:49 +0900

    [FEAT] 비밀번호 변경 기능 틀 추가 #3

--- src/components/MyPage/ChangePasswordModal.tsx
+++ src/components/MyPage/ChangePasswordModal.tsx
@@ -5,13 +5,22 @@ type ModalProps = {
 };
 
 export function ChangePasswordModal({ resetModal }: ModalProps) {
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const password = formData.get('password');
+    console.log('비밀번호 변경', password);
+  }
+
   return (
     <ModalContainer resetModal={resetModal}>
       <h1>비밀번호 변경</h1>
       <div>비밀번호를 입력해주세요.</div>
-      <input type='password' />
-      <button>확인</button>
-      <button>취소</button>
+      <form onSubmit={onSubmit}>
+        <input type='password' />
+        {/* NOTE : 비밀번호 확인 입력이 있을수도 있음 */}
+        <button type='submit'>확인</button>
+      </form>
     </ModalContainer>
   );
 }
