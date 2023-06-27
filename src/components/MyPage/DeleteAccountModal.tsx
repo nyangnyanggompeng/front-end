1637ef8f6518e555e9cf059b96c6143b8f39281c
yoouyeon <jyeon.yoon59@gmail.com>
2023-06-27 22:36:44 +0900

    [FEAT] 회원 탈퇴 모달 #3

--- src/components/MyPage/DeleteAccountModal.tsx
+++ src/components/MyPage/DeleteAccountModal.tsx
@@ -5,12 +5,20 @@ type ModalProps = {
 };
 
 export function DeleteAccountModal({ resetModal }: ModalProps) {
+  function onClick(e: React.MouseEvent<Element, MouseEvent>) {
+    e.preventDefault();
+    console.log('회원 탈퇴');
+  }
+  function onCancel(e: React.MouseEvent<Element, MouseEvent>) {
+    e.preventDefault();
+    console.log('회원 탈퇴 취소');
+  }
   return (
     <ModalContainer resetModal={resetModal}>
       <h1>회원 탈퇴</h1>
       <div>정말로 탈퇴하시겠습니까?</div>
-      <button>예</button>
-      <button>아니오</button>
+      <button onClick={onClick}>예</button>
+      <button onClick={onCancel}>아니오</button>
     </ModalContainer>
   );
 }
