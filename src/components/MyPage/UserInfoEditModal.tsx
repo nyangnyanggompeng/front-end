f435ad6c073bd024a097e3fa4c2e7047bf536791
yoouyeon <jyeon.yoon59@gmail.com>
2023-06-27 23:45:31 +0900

    [FEAT] 프로필 변경 모달 기능 틀 추가 #3

--- src/components/MyPage/UserInfoEditModal.tsx
+++ src/components/MyPage/UserInfoEditModal.tsx
@@ -6,13 +6,24 @@ type ModalProps = {
 };
 
 export function UserInfoEditModal({ resetModal }: ModalProps) {
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const profileImage = formData.get('profile-image');
+    const nickname = formData.get('nickname');
+    console.log('프로필 사진 변경', profileImage);
+    console.log('닉네임 변경', nickname);
+  }
+
   return (
     <ModalContainer resetModal={resetModal}>
       <h1>정보 수정</h1>
       <div>프로필 사진</div>
-      <input type='file' />
-      <NicknameCheck />
-      <button>정보 수정</button>
+      <form onSubmit={onSubmit}>
+        <input name='profile-image' type='file' />
+        <NicknameCheck />
+        <button type='submit'>정보 수정</button>
+      </form>
     </ModalContainer>
   );
 }
