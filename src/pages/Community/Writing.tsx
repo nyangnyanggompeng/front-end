62909fd3f5c3e3ec8007a76904f53ecdcba215c4
yoouyeon <jyeon.yoon59@gmail.com>
2023-06-29 05:17:12 +0900

    [FEAT] 뒤로가기, 새로고침 이벤트 처리 #9

--- src/pages/Community/Writing.tsx
+++ src/pages/Community/Writing.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { CancelModal } from '../../components/Community/Writing/components/CancelModal';
@@ -6,6 +6,20 @@ import { CancelModal } from '../../components/Community/Writing/components/Cance
 export function Writing() {
   const [cancelModalOpen, setCancelModalOpen] = useState(false);
   const editorRef = useRef<Editor>(null);
+
+  useEffect(() => {
+    // 뒤로가기, 새로고침, 창 끄기 등 현재 페이지를 벗어나는 경우에 경고 alert 띄우기
+    window.addEventListener('beforeunload', beforeunloadHandler);
+    return () => {
+      window.removeEventListener('beforeunload', beforeunloadHandler);
+    };
+  }, []);
+
+  function beforeunloadHandler(e: BeforeUnloadEvent) {
+    e.preventDefault();
+    e.returnValue = '';
+    console.log('beforeunload');
+  }
 
   function handleClick() {
     if (editorRef.current) {
@@ -15,6 +29,10 @@ export function Writing() {
 
   return (
     <>
+      {/* NOTE : Custom Modal은 오직 직접 취소하기 버튼을 눌러 창을 벗어나는 경우에만 사용됨. 
+        대안 1 : 뒤로가기, 새로고침, 창 끄기 등 현재 페이지를 벗어나는 경우에 Custom Modal만을 띄울 수 있는 방법 찾아보기
+        대안 2 : 취소하기 버튼을 누른 경우에도 Custom Modal 대신 브라우저 alert창으로 대체하기
+      */}
       {cancelModalOpen && (
         <CancelModal resetModal={() => setCancelModalOpen(false)} />
       )}
