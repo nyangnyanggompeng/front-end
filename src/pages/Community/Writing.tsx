821c57c69840159442fca1ff84b22c9d0426a2a1
yoouyeon <jyeon.yoon59@gmail.com>
2023-06-29 02:18:48 +0900

    [FEAT] Writing 페이지 기능 틀 추가 #9

--- src/pages/Community/Writing.tsx
+++ src/pages/Community/Writing.tsx
@@ -1,12 +1,34 @@
+import { useRef } from 'react';
+import { Editor } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
 export function Writing() {
+  const editorRef = useRef<Editor>(null);
+
+  function handleClick() {
+    if (editorRef.current) {
+      console.log(editorRef.current.getInstance().getMarkdown());
+    }
+  }
+
+  function handleCancel() {
+    // TODO : 모달 띄워서 확인하고 취소하기
+    console.log('취소하기');
+  }
+
   return (
     <div>
       <div>글쓰기</div>
-      <div>
-        <input type='text' placeholder='제목' />
-        <textarea placeholder='내용' />
-      </div>
-      <button>작성</button>
+      <input type='text' placeholder='제목' />
+      <Editor
+        ref={editorRef}
+        previewStyle='vertical'
+        height='600px'
+        initialEditType='wysiwyg'
+        placeholder='글을 작성해주세요.' // TODO : 글자수 제한을 둘 수 있을지 확인해보기
+      />
+      <button onClick={handleCancel}>취소하기</button>
+      <button onClick={handleClick}>등록하기</button>
     </div>
   );
 }
