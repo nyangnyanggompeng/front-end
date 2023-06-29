db3e1459c2cf79a868e1d45d4ceca9b5effdb1b0
yoouyeon <jyeon.yoon59@gmail.com>
2023-06-30 04:40:57 +0900

    [FEAT] 게시글 상세 조회 라우팅 설정 #10

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import MyPage from './pages/MyPage';
 import SignIn from './pages/SignIn';
+import { Article } from './pages/Article';
 
 function Router() {
   return (
@@ -11,6 +12,7 @@ function Router() {
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/my-page' element={<MyPage />} />
         <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/article/:id' element={<Article />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Article.tsx
@@ -0,0 +1,7 @@
+export function Article() {
+  return (
+    <div>
+      <h1>Article</h1>
+    </div>
+  );
+}
