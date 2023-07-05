e498be8036809a9bd2327cd36be3063ec2caf0a3
yoouyeon <jyeon.yoon59@gmail.com>
2023-07-05 12:42:34 +0900

    [FIX] postType 타입 수정 #14

--- src/types/Community/communityTypes.ts
+++ src/types/Community/communityTypes.ts
@@ -1,11 +1,9 @@
-// TODO : updateAt, createAt 확인하기
-// TODO : snake_case와 camelCase 통일 얘기해보기
 export type postType = {
   id: number;
   title: string;
   content: string;
   writer: string;
-  updatedAt: string;
-  num_of_comment: number;
-  user_id: number;
+  createdAt: string;
+  numOfComment: number;
+  userId: number;
 };
