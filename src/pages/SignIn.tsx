0de6250466c2acc64bef3929f26a32a5a3387571
moondrop0816 <moondrop0816@gmail.com>
2023-07-06 15:01:58 +0900

    [CHORE] 에러메시지 작성 및 분기 #2

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,15 +1,27 @@
 import axios from 'axios';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 interface LoginInfo {
   userId: string;
   password: string;
 }
 
+interface Message {
+  [key: string]: string;
+}
+
 const SignIn = () => {
   const navigate = useNavigate();
-  const [error, setError] = useState(false);
+  const message: Message = {
+    LOGIN_SUCCESS: '',
+    LOGIN_FAILURE: '이메일 또는 비밀번호가 잘못 입력되었습니다.',
+    EMAIL_DOESNT_EXISTS: '존재하지 않는 사용자입니다.',
+    EMAIL_OR_PASSWORD_NOT_ENTERED:
+      '이메일 또는 비밀번호가 잘못 입력되었습니다.',
+    SERVER_ERROR: '서버 오류입니다. 잠시 후 다시 시도해 주세요.',
+  };
+  const [error, setError] = useState('');
   const [loginInfo, setLoginInfo] = useState<LoginInfo>({
     userId: '',
     password: '',
@@ -29,7 +41,7 @@ const SignIn = () => {
 
     // 입력된 값 유효성 검사
     if (!loginInfo.userId || !loginInfo.password) {
-      setError(true);
+      setError(message.EMAIL_OR_PASSWORD_NOT_ENTERED);
       return;
     }
 
@@ -41,24 +53,25 @@ const SignIn = () => {
       password: loginInfo.password,
     };
 
-    console.log(data);
-
-    // 액세스 토큰 발급(로그인)
+    // 액세스 토큰 발급
     await axios
       .post('/users/login', {
-        data,
+        ...data,
       })
       .then((res) => {
         console.log(res.data);
-
-        // const { accessToken } = res.data;
-        // console.log(accessToken);
-        // // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
-        // axios.defaults.headers.common[
-        //   'Authorization'
-        // ] = `Bearer ${accessToken}`;
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        // 400대 에러
+        if (err.response.status === 400) {
+          setError(message[err.response.data]);
+        }
+
+        // 500대 에러
+        if (err.response.status / 500 === 1) {
+          setError(message.SERVER_ERROR);
+        }
+      });
 
     // 발급된 토큰으로 유저정보 가져오기
     // await axios
@@ -95,16 +108,14 @@ const SignIn = () => {
             onChange={handleOnChange}
           />
         </div>
-        {error && (
-          <p className='err-msg'>이메일 또는 비밀번호가 잘못 입력되었습니다.</p>
-        )}
+        {error && <p className='err-msg'>{error}</p>}
         <button type='submit'>로그인</button>
       </form>
       <p>
         비밀번호를 잊으셨나요?<a href='#'>비밀번호 찾기</a>
       </p>
       <p>
-        아직 회원이 아니신가요?<a href='#'>회원가입</a>
+        아직 회원이 아니신가요?<Link to='/sign-up'>회원가입</Link>
       </p>
     </div>
   );
