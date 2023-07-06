b3c5d5584e755952aa5126f867b49036fe027086
moondrop0816 <moondrop0816@gmail.com>
2023-07-07 00:15:40 +0900

    [FEAT] 상태별 버튼 컴포넌트 구현 #20

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -1,23 +1,46 @@
-import { css } from '@emotion/react';
-import theme from '../../theme';
+import { Theme, css } from '@emotion/react';
 
-const StyledBtn = css({
-  display: 'inline-block',
-  color: `${theme.colors.light.primary}`,
-});
+// theme의 type을 Theme로 설정했더니 에러가 나서 임시로 any 타입 줌
+const StyledBtn = (theme: any) =>
+  css({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '1rem',
+    color: `${theme.white}`,
+    borderRadius: '0.5rem',
+    backgroundColor: `${theme.orange1}`,
+    padding: '1rem 2rem',
+
+    '&.sub': {
+      color: `${theme.orange1}`,
+      backgroundColor: `${theme.white}`,
+      border: `1px solid ${theme.orange1}`,
+    },
+
+    '&.disable': {
+      backgroundColor: `${theme.gray2}`,
+      cursor: 'default',
+    },
+  });
 
 interface BtnProps {
   children: React.ReactNode;
   type?: 'button' | 'submit' | 'reset';
   onClick(): void;
+  status?: 'main' | 'sub' | 'disable';
 }
 
 const Button = (props: BtnProps) => {
-  // 주버튼, 부버튼, 비활성화
-  // 아이콘 있음 없음
-  const { children, type = 'button', onClick } = props;
+  const { children, type = 'button', onClick, status = 'main' } = props;
   return (
-    <button type={type} onClick={onClick} css={StyledBtn}>
+    <button
+      type={type}
+      onClick={onClick}
+      css={StyledBtn}
+      className={status}
+      disabled={status === 'disable' ? true : false}
+    >
       {children}
     </button>
   );
