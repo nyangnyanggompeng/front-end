4b6ca697d5e42d6f562d997480d6065eb2ed4e09
moondrop0816 <moondrop0816@gmail.com>
2023-07-06 23:56:13 +0900

    [FEAT] 라이트, 다크모드 컬러 테마 설정 #20

--- src/emotion.d.ts
+++ src/emotion.d.ts
@@ -1,17 +1,16 @@
 import '@emotion/react';
-import { theme } from './theme';
-
-type ThemeType = typeof theme;
 
 declare module '@emotion/react' {
-  export interface Theme extends ThemeType {
-    colors: {
-      light: {
-        primary: string;
-      };
-      dark: {
-        primary: string;
-      };
-    };
+  export interface ColorMode {
+    blue1: string;
+    blue2: string;
+    orange1: string;
+    orange2: string;
+    yellow: string;
+    green: string;
+    gray1: string;
+    gray2: string;
+    black: string;
+    white: string;
   }
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,16 +1,28 @@
-const light = {
-  primary: 'blue',
+import { ColorMode } from '@emotion/react';
+
+export const lightMode: ColorMode = {
+  blue1: '#7ac8ec',
+  blue2: '#019ac4',
+  orange1: '#e87701',
+  orange2: '#ec9d04',
+  yellow: '#f9d283',
+  green: '#88b837',
+  gray1: '#aaa',
+  gray2: '#ccc',
+  black: '#333',
+  white: '#fff',
 };
 
-const dark = {
-  primary: 'orange',
+// TODO : 다크모드 적용시 색상 변경 예정
+export const darkMode: ColorMode = {
+  blue1: '#7ac8ec',
+  blue2: '#019ac4',
+  orange1: '#e87701',
+  orange2: '#ec9d04',
+  yellow: '#f9d283',
+  green: '#88b837',
+  gray1: '#aaa',
+  gray2: '#ccc',
+  black: '#333',
+  white: '#fff',
 };
-
-const theme = {
-  colors: {
-    light,
-    dark,
-  },
-};
-
-export default theme;
