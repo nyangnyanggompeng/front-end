bcb129a6b788962104380af52af018932c740882
yoouyeon <jyeon.yoon59@gmail.com>
2023-07-07 18:06:44 +0900

    [FIX] 변경된 api 타입에 맞게 수정 #10

--- src/types/Community/communityTypes.ts
+++ src/types/Community/communityTypes.ts
@@ -1,7 +1,15 @@
-export type postType = {
+export type ArticleType = {
   id: number;
   title: string;
   content: string;
   writer: string;
   createdAt: string;
+  numOfComment: number;
+  userId: number;
+};
+
+export type ArticleDataType = {
+  post: ArticleType[];
+  numberOfPost: number;
+  totalPages: number;
 };
