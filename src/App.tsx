fa5faa4dfdfe6f4aa7222e1525b15491c0de1f73
yoouyeon <jyeon.yoon59@gmail.com>
2023-07-08 02:36:15 +0900

    [FIX] ngrok get error 수정 #10

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ function App() {
   // axios 전역 기본값 설정
   axios.defaults.baseURL = VITE_SERVER_URL;
   axios.defaults.headers.post['Content-Type'] = 'application/json';
+  // NOTE : ngrok get 요청 error 해결을 위해 추가한 헤더. 추후 삭제 필요합니다.
+  axios.defaults.headers.get['ngrok-skip-browser-warning'] = '69420';
   axios.defaults.withCredentials = true;
-
   return (
     <>
       <Router />
