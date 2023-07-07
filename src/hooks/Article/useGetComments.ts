fc13445bd6686f69da68dff786a0e41a9ce7a181
yoouyeon <jyeon.yoon59@gmail.com>
2023-07-08 03:05:40 +0900

    [FEAT] useGetComments 훅 추가 #10

---
+++ src/hooks/Article/useGetComments.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { CommentDataType } from '../../types/Community/commentTypes';
+import { getComments } from '../../utils/Community/getComments';
+
+export default function useGetComments(currentPage: number, postId?: string) {
+  const [commentData, setCommentData] = useState<CommentDataType | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  useEffect(() => {
+    if (!postId) {
+      setCommentData(null);
+      alert('post id가 없습니다!');
+      return;
+    }
+    setIsLoading(true);
+    getComments(parseInt(postId), currentPage)
+      .then((commentData: CommentDataType) => setCommentData(commentData))
+      .catch(() => {
+        setCommentData(null);
+        alert('서버 오류입니다. 잠시 후 다시 시도해주세요.');
+      })
+      .finally(() => setIsLoading(false));
+  }, [postId, currentPage]);
+  return { commentData, isLoading };
+}
