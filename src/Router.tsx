74a2bcd67311c79d99c1a73dbe64e4d112335552
moondrop0816 <moondrop0816@gmail.com>
2023-07-09 00:33:03 +0900

    [CHORE] BrowserRouter 위치 수정 #20

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,16 +3,16 @@ import { Community, MyPage, SignIn, SignUp, Writing } from './pages';
 
 function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path='/' element={<Home />} /> */}
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/my-page' element={<MyPage />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/community' element={<Community />} />
-        <Route path='/writing' element={<Writing />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    <Routes>
+      {/* <Route path='/' element={<Home />} /> */}
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/my-page' element={<MyPage />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/community' element={<Community />} />
+      <Route path='/writing' element={<Writing />} />
+    </Routes>
+    // </BrowserRouter>
   );
 }
 
