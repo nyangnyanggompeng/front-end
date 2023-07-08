1e5c17e535947df65946983abb108e3e674660b2
yoouyeon <jyeon.yoon59@gmail.com>
2023-07-08 15:37:32 +0900

    [CHORE] 작업 예정 내용 주석 추가 #14

--- src/utils/Writing/postArticle.ts
+++ src/utils/Writing/postArticle.ts
@@ -7,8 +7,10 @@ import {
 export async function postArticle(
   newArticle: ArticleType
 ): Promise<WritingStatusType> {
+  // TODO : 현재 로그인한 유저의 Id, 임시로 넣은 것이므로 추후 삭제 필요함.
+  const user_id = 1;
   try {
-    await axios.post('/board/1', newArticle);
+    await axios.post(`/board/${user_id}`, newArticle);
     return 'SUCCESS';
   } catch (error: unknown) {
     if (isAxiosError(error)) {
