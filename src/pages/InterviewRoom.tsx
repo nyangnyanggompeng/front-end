965997ab6ac7a20b1f90dcfc5fa10277f3047dc1
moondrop0816 <moondrop0816@gmail.com>
2023-07-10 00:50:10 +0900

    [CHORE] 새 인터뷰 등록 모달 작성 #24

--- src/pages/InterviewRoom.tsx
+++ src/pages/InterviewRoom.tsx
@@ -5,6 +5,7 @@ import { faTrashCan } from '@fortawesome/free-regular-svg-icons';
 import InterviewItem from '../components/Interview/InterviewItem';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { ModalContainer } from '../components/Modal/ModalContainer';
 
 interface InterviewListData {
   id: number;
@@ -42,6 +43,7 @@ const InterviewRoom = () => {
   const [interviewList, setInterviewList] = useState<InterviewListData[] | []>(
     []
   );
+  const [isOpen, setIsOpen] = useState(false);
 
   const getList = async () => {
     try {
@@ -58,52 +60,69 @@ const InterviewRoom = () => {
   }, []);
 
   return (
-    <main>
-      <div className='inner'>
-        <h2>인터뷰 룸</h2>
-        <div className='subtit'>
-          <div className='left'>
-            <h3>전체 {dummy.numberOfList}개</h3>
-            <h4>채팅방은 최대 30개까지 생성할 수 있습니다.</h4>
-          </div>
-          <div>검색</div>
-        </div>
-        <div className='btns'>
-          <Button status='sub' onClick={() => console.log('전체삭제')}>
-            <FontAwesomeIcon icon={faTrashCan} />
-            전체삭제
-          </Button>
-          <Button status='sub' onClick={() => console.log('선택삭제')}>
-            <FontAwesomeIcon icon={faEraser} />
-            선택삭제
-          </Button>
-          <Button onClick={() => console.log('선택삭제')}>
-            <FontAwesomeIcon icon={faPlus} />새 인터뷰
-          </Button>
-        </div>
-        <ul className='interview-list'>
-          {interviewList.length === 0 ? (
-            <div>
-              인터뷰 룸이 존재하지 않습니다. <br />
-              새로운 인터뷰 룸을 생성해 보세요!
+    <>
+      <main>
+        <div className='inner'>
+          <h2>인터뷰 룸</h2>
+          <div className='subtit'>
+            <div className='left'>
+              <h3>전체 {dummy.numberOfList}개</h3>
+              <h4>채팅방은 최대 30개까지 생성할 수 있습니다.</h4>
             </div>
-          ) : (
-            interviewList.map((item) => {
-              return (
-                <InterviewItem
-                  key={item.id}
-                  type={item.type}
-                  title={item.name}
-                  createdAt={item.createdAt}
-                />
-              );
-            })
-          )}
-        </ul>
-        {/* TODO : 페이지네이션 추가 */}
-        <div>pagenation</div>
-      </div>
-    </main>
+            <div>검색</div>
+          </div>
+          <div className='btns'>
+            <Button status='sub' onClick={() => console.log('전체삭제')}>
+              <FontAwesomeIcon icon={faTrashCan} />
+              전체삭제
+            </Button>
+            <Button status='sub' onClick={() => console.log('선택삭제')}>
+              <FontAwesomeIcon icon={faEraser} />
+              선택삭제
+            </Button>
+            <Button onClick={() => setIsOpen(true)}>
+              <FontAwesomeIcon icon={faPlus} />새 인터뷰
+            </Button>
+          </div>
+          <ul className='interview-list'>
+            {interviewList.length === 0 ? (
+              <div>
+                인터뷰 룸이 존재하지 않습니다. <br />
+                새로운 인터뷰 룸을 생성해 보세요!
+              </div>
+            ) : (
+              interviewList.map((item) => {
+                return (
+                  <InterviewItem
+                    key={item.id}
+                    type={item.type}
+                    title={item.name}
+                    createdAt={item.createdAt}
+                  />
+                );
+              })
+            )}
+          </ul>
+          {/* TODO : 페이지네이션 추가 */}
+          <div>pagenation</div>
+        </div>
+      </main>
+      {isOpen && (
+        <ModalContainer resetModal={() => setIsOpen(!isOpen)}>
+          <h2>새 인터뷰 만들기</h2>
+          <div className='input-box'>
+            <label htmlFor='newInterviewName'>인터뷰 이름</label>
+            <input type='text' id='newInterviewName' />
+          </div>
+          <div className='btn-box'>
+            <Button status='sub' onClick={() => setIsOpen(false)}>
+              취소
+            </Button>
+            <Button onClick={() => console.log('인터뷰 생성')}>완료</Button>
+          </div>
+        </ModalContainer>
+      )}
+    </>
   );
 };
 
