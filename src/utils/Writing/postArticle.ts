ce023791ae0a5a4687060e2a36dcb89f10dee074
yoouyeon <jyeon.yoon59@gmail.com>
2023-07-10 03:04:43 +0900

    [CHORE] 임시 로그인 유저 id 존재하는 값으로 수정 #14

--- src/utils/Writing/postArticle.ts
+++ src/utils/Writing/postArticle.ts
@@ -8,7 +8,7 @@ export async function postArticle(
   newArticle: ArticleType
 ): Promise<WritingStatusType> {
   // TODO : 현재 로그인한 유저의 Id, 임시로 넣은 것이므로 추후 삭제 필요함.
-  const user_id = 1;
+  const user_id = 10;
   try {
     await axios.post(`/board/${user_id}`, newArticle);
     return 'SUCCESS';
