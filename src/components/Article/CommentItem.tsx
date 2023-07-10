ccd94ba74540cc4d0155e985f9e117fc2f85757c
yoouyeon <jyeon.yoon59@gmail.com>
2023-07-10 10:40:50 +0900

    [FEAT] 댓글 수정 기능 틀 추가 #10

--- src/components/Article/CommentItem.tsx
+++ src/components/Article/CommentItem.tsx
@@ -2,6 +2,7 @@ import { useQueryClient } from '@tanstack/react-query';
 import { CommentType } from '../../types/Community/commentTypes';
 import { getDate } from '../../utils/Common/getDate';
 import { deleteComments } from '../../utils/Community/deleteComments';
+import { useState } from 'react';
 
 type CommentItemProps = {
   comment: CommentType;
@@ -9,6 +10,7 @@ type CommentItemProps = {
 };
 
 export default function CommentItem({ comment, postId }: CommentItemProps) {
+  const [isEdit, setIsEdit] = useState<boolean>(false);
   // TODO : 현재 로그인된 유저의 id가 필요함.
   const currentUserId = 10;
   const queryClient = useQueryClient();
@@ -24,17 +26,37 @@ export default function CommentItem({ comment, postId }: CommentItemProps) {
       });
   }
 
+  function onEditHander(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const newComment = formData.get('newComment') as string;
+    if (!newComment) {
+      alert('댓글을 작성해주세요.');
+      return;
+    }
+    // TODO : 수정된 댓글을 서버에 보내는 함수 필요함.
+    setIsEdit(false);
+  }
+
   return (
     <li>
       <div>{comment.writer}</div>
       <div>{getDate(new Date(comment.createdAt))}</div>
       {currentUserId === comment.userId && (
         <div>
-          <button>수정</button>
+          <button onClick={() => setIsEdit(true)}>수정</button>
           <button onClick={onDeleteHander}>삭제</button>
         </div>
       )}
-      <div>{comment.content}</div>
+      {isEdit ? (
+        <form onSubmit={onEditHander}>
+          <input></input>
+          <button>작성 완료</button>
+          <button onClick={() => setIsEdit(false)}>수정 취소</button>
+        </form>
+      ) : (
+        <div>{comment.content}</div>
+      )}
     </li>
   );
 }
