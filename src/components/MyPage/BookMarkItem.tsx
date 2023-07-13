acfca28ebf3007954d5c385ed25929ef0a35764d
yoouyeon <jyeon.yoon59@gmail.com>
2023-07-13 12:31:31 +0900

    [FEAT] 마이페이지 북마크 해제 기능 #16

--- src/components/MyPage/BookMarkItem.tsx
+++ src/components/MyPage/BookMarkItem.tsx
@@ -1,15 +1,23 @@
 import { Link } from 'react-router-dom';
+import { useQueryClient } from '@tanstack/react-query';
 import { BookMarkType } from '../../types/MyPage/BookMarkTypes';
+import { deleteBookmark } from '../../utils/Common/deleteBookmark';
 
 type BookMarkItemProps = {
   bookmark: BookMarkType;
 };
 
 export default function BookMarkItem({ bookmark }: BookMarkItemProps) {
+  const queryClient = useQueryClient();
   function deleteHandler(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
-    // TODO : 북마크 api 작업시에 북마크 해제 기능 추가 예정입니다.
-    console.log('북마크 해제');
+    window.confirm('북마크를 해제하시겠습니까?') &&
+      deleteBookmark(bookmark.id)
+        .then(() => {
+          queryClient.invalidateQueries({ queryKey: ['bookmarks'] });
+          alert('북마크 해제 완료');
+        })
+        .catch(() => alert('북마크 해제 실패. 다시 시도해주세요.'));
   }
 
   return (
