be27e86ad2af872a907a1140e0c8aed9f8b69ef0
moondrop0816 <moondrop0816@gmail.com>
2023-07-18 02:34:23 +0900

    [DEL] 불필요한 파일 삭제 #2

--- src/utils/SignIn/userFunc.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// 로그아웃 로직
-export const logoutHandler = () => {
-  return axios
-    .get('/users/logout')
-    .then((res) => console.log('로그아웃시 상태변경'))
-    .catch((err) => console.log('로그아웃 실패 로직'));
-};
