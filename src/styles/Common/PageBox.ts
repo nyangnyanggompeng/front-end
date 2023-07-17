6a22bc6e20cc40eea81c585244eb7182b92a3c29
yoouyeon <jyeon.yoon59@gmail.com>
2023-07-17 21:12:22 +0900

    [DESIGN] Box 형태의 페이지 Wrapper 디자인 추가 #28

---
+++ src/styles/Common/PageBox.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+import { Theme } from '@emotion/react';
+
+export const PageBox = (theme: Theme) =>
+  css({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: 'fit-content',
+    maxWidth: '45rem',
+    padding: '3.3rem 3.1rem',
+    margin: '0 auto',
+    border: `1px solid ${theme.black}`,
+    '*': {
+      width: '100%',
+    },
+  });
