b0d82e96d4711c811af604c4acd9e4effc042b0e
moondrop0816 <moondrop0816@gmail.com>
2023-07-17 22:48:53 +0900

    [FEAT] 로그인 페이지 기본 디자인 적용 #2

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -2,6 +2,9 @@ import axios from 'axios';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { logoutHandler } from '../utils/SignIn/userFunc';
+import { Theme, css, useTheme } from '@emotion/react';
+import logo from '../asset/logo.png';
+import Button from '../components/Common/Button';
 
 interface LoginInfo {
   userId: string;
@@ -12,7 +15,61 @@ interface Message {
   [key: string]: string;
 }
 
+const StyledSignIn = (theme: Theme) =>
+  css({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    '.signin-wrap': {
+      border: `1px solid ${theme.gray2}`,
+      borderRadius: 5,
+      padding: '5rem',
+      width: '50%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      img: {
+        alignSelf: 'center',
+        marginBottom: '3rem',
+      },
+      form: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'stretch',
+        marginBottom: '3rem',
+        '> div:first-of-type': {
+          marginBottom: '2rem',
+        },
+        label: {
+          fontWeight: 700,
+          fontSize: '2rem',
+          marginBottom: '0.5rem',
+          display: 'block',
+        },
+        '.err-msg': {
+          color: `${theme.orange1}`, // FIXME
+          fontSize: '1.4rem',
+          marginTop: '0.5rem',
+        },
+        button: {
+          marginTop: '3rem',
+        },
+      },
+      '> p': {
+        textAlign: 'center',
+        marginBottom: '1rem',
+        a: {
+          color: `${theme.orange1}`,
+          fontWeight: 700,
+          marginLeft: '1.5rem',
+        },
+      },
+    },
+  });
+
 const SignIn = () => {
+  const theme = useTheme();
   const navigate = useNavigate();
   const message: Message = {
     LOGIN_SUCCESS: '',
@@ -37,8 +94,8 @@ const SignIn = () => {
     });
   };
 
-  const loginRequestHandler = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const loginRequestHandler = async () => {
+    // e.preventDefault();
 
     // 입력된 값 유효성 검사
     if (!loginInfo.userId || !loginInfo.password) {
@@ -88,41 +145,44 @@ const SignIn = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={loginRequestHandler}>
-        <div>
-          <label htmlFor='userId'>이메일</label>
-          <input
-            type='text'
-            id='userId'
-            placeholder='abcd@email.com'
-            value={loginInfo.userId}
-            onChange={handleOnChange}
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>비밀번호</label>
-          <input
-            type='password'
-            id='password'
-            placeholder='password'
-            value={loginInfo.password}
-            onChange={handleOnChange}
-          />
-        </div>
-        {error && <p className='err-msg'>{error}</p>}
-        <button type='submit'>로그인</button>
-      </form>
-      <button type='button' onClick={logoutHandler}>
-        로그아웃
-      </button>
-      <p>
-        비밀번호를 잊으셨나요?<a href='#'>비밀번호 찾기</a>
-      </p>
-      <p>
-        아직 회원이 아니신가요?<Link to='/sign-up'>회원가입</Link>
-      </p>
-    </div>
+    <main css={StyledSignIn(theme)} className='inner'>
+      <div className='signin-wrap'>
+        <img src={logo} alt='인터뷰 연구소' />
+        <form onSubmit={loginRequestHandler}>
+          <div>
+            <label htmlFor='userId'>이메일</label>
+            <input
+              type='text'
+              id='userId'
+              placeholder='abcd@email.com'
+              value={loginInfo.userId}
+              onChange={handleOnChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='password'>비밀번호</label>
+            <input
+              type='password'
+              id='password'
+              placeholder='password'
+              value={loginInfo.password}
+              onChange={handleOnChange}
+            />
+          </div>
+          <p className='err-msg'>에러메시지에용</p>
+          {error && <p className='err-msg'>{error}</p>}
+          <Button type='submit' onClick={loginRequestHandler}>
+            로그인
+          </Button>
+        </form>
+        <p>
+          비밀번호를 잊으셨나요?<a href='#'>비밀번호 찾기</a>
+        </p>
+        <p>
+          아직 회원이 아니신가요?<Link to='/sign-up'>회원가입</Link>
+        </p>
+      </div>
+    </main>
   );
 };
 
