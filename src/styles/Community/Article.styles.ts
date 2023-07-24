d9bd738b1063cc0c712b4b8d809b26818a51b9e8
yoouyeon <jyeon.yoon59@gmail.com>
2023-07-24 14:26:43 +0900

    [DESIGN] 게시물 목록 항목 비율 조절 #28

--- src/styles/Community/Article.styles.ts
+++ src/styles/Community/Article.styles.ts
@@ -4,8 +4,8 @@ import { OverflowEllipsis } from '../utils';
 export const ArticleItemContainer = (theme: Theme) =>
   css({
     display: 'grid',
-    gridTemplateColumns: '10fr 2fr 1fr 1fr',
-    gridGap: '6.5rem',
+    gridTemplateColumns: '9fr 2fr 2fr 1fr',
+    gridGap: '4rem',
     border: `1px solid ${theme.black}`,
     borderRadius: '0.5rem', // NOTE: small radius
     padding: '1.25rem',
