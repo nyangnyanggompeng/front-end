7b3e5d4de58691d6da67cd43131531bdeeaf99c8
yoouyeon <jyeon.yoon59@gmail.com>
2023-07-26 03:11:12 +0900

    [CHORE] Commit 빠진 내용 보충 #32

--- src/components/Article/Viewer.tsx
+++ src/components/Article/Viewer.tsx
@@ -1,9 +1,18 @@
+import { useTheme } from '@emotion/react';
 import { Viewer as TuiViewer } from '@toast-ui/react-editor';
+import { ViewerWrapper } from '../../styles/Community';
+import { useIsDark } from '../../hooks/Common';
 
 type viewerProps = {
   content: string;
 };
 
 export function Viewer({ content }: viewerProps) {
-  return <TuiViewer initialValue={content} />;
+  const theme = useTheme();
+  const isDark = useIsDark();
+  return (
+    <div css={ViewerWrapper(theme)}>
+      <TuiViewer initialValue={content} />
+    </div>
+  );
 }
